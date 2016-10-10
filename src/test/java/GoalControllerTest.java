5b9a9021582e6ffd9a96a1614669f7e106d302fb
Filip T <ftancula@gmail.com>
2016-10-10 16:46:10 +0200

    created test for getOneGoal method in GoalController

--- src/test/java/GoalControllerTest.java
+++ src/test/java/GoalControllerTest.java
@@ -1,22 +1,36 @@
 import com.ftanc.controller.GoalController;
 import com.ftanc.model.Goal;
+import com.ftanc.repository.GoalRepository;
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
-import java.util.List;
-
-static import
-
-import java.util.List;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
 
 /**
  * Created by ftanc on 10/10/2016.
  */
 public class GoalControllerTest {
+    @InjectMocks
+    private GoalController goalController;
+
+    @Mock
+    private GoalRepository goalRepository;
+
+    @Before
+    public void init(){
+        MockitoAnnotations.initMocks(this);
+    }
 
     @Test
     public void testGoalGet(){
-        GoalController controller = new GoalController();
-        List<Goal> goal = controller.getAllGoals();
-        assertEquals
+        Goal mockedGoal = new Goal();
+        mockedGoal.setId(1);
+        when(goalRepository.findOne(1L)).thenReturn(mockedGoal);
+        Goal goal = goalController.getOneGoal(1L);
+        assertEquals(1L, goal.getId());
     }
 }
